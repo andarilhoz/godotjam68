8b32f9e8ffa223e13242b4f3569887f45c7827b4
Magno Gouveia <owlflee@gmail.com>
2024-04-15 13:33:56 -0300

    Fix menu problems

--- menu/options_menu.gd
+++ menu/options_menu.gd
@@ -4,4 +4,4 @@ func _on_volume_pressed():
 	pass # Replace with function body.
 
 func _on_back_pressed():
-	get_tree().change_scene_to_file("res://menu.tscn")
+	get_tree().change_scene_to_file("res://menu/menu.tscn")
