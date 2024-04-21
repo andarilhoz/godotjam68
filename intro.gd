a846fbd3a99c7f3f59396fb5d4b3ddca3e175f64
Magno Gouveia <owlflee@gmail.com>
2024-04-21 06:24:54 -0300

    Making tutorial

---
+++ intro.gd
@@ -0,0 +1,16 @@
+extends Control
+
+@onready var first_panel : TextureRect = $Control/Panel/TextureRect
+@onready var second_panel : TextureRect = $Control/Panel/TextureRect2
+
+
+@onready var previous_btn: Button = $Control/Panel/Previous
+@onready var back_btn: Button = $Control/Panel/Back
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
